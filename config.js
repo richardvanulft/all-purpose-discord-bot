157da04645755caf02995e69a80af1818f55339f
Richard <richard@gaaf-online.nl>
2024-06-05 14:03:27 +0200

    new functions

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
-    CONTEXT: false, // Should contexts be enabled
-    GLOBAL: false, // Should the interactions be registered globally
+    CONTEXT: true, // Should contexts be enabled
+    GLOBAL: true, // Should the interactions be registered globally
     TEST_GUILD_ID: "1236968900516646973", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
