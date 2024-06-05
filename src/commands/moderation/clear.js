317139a0da39ee34c4012f6f2c32d85d0dc87c94
Richard <richard@gaaf-online.nl>
2024-06-05 13:42:35 +0200

    new functions

--- src/commands/moderation/clear.js
+++ src/commands/moderation/clear.js
@@ -24,7 +24,7 @@ module.exports = {
     const moderatorRoleID = process.env.MODERATOR_ROLE_ID;
 
     if (!interaction.member.roles.cache.has(moderatorRoleID)) {
-      return interaction.reply('You do not have permission to use this command.');
+      return interaction.followUp('You do not have permission to use this command.');
     }
     const amount = interaction.options.getString("amount");
 
@@ -35,25 +35,25 @@ module.exports = {
       do {
         batch = await interaction.channel.bulkDelete(100, true).catch(err => {
           if (err.message.includes("You can only bulk delete messages that are under 14 days old.")) {
-            interaction.reply("I cannot delete messages that are older than 14 days.");
+            interaction.followUp("I cannot delete messages that are older than 14 days.");
           } else {
-            interaction.reply("There was an error trying to prune messages in this channel!");
+            interaction.followUp("There was an error trying to prune messages in this channel!");
           }
         });
         if (batch) deleted += batch.size;
       } while (batch && batch.size === 100);
-      if (deleted) interaction.reply(`Successfully deleted ${deleted} messages.`);
+      if (deleted) interaction.followUp(`Successfully deleted ${deleted} messages.`);
     } else {
       // Delete specified amount of messages
       const count = parseInt(amount);
       if (isNaN(count) || count < 1 || count > 100) {
-        interaction.reply("You need to input a number between 1 and 100.");
+        interaction.followUp("You need to input a number between 1 and 100.");
       } else {
         await interaction.channel.bulkDelete(count, true).catch(err => {
           if (err.message.includes("You can only bulk delete messages that are under 14 days old.")) {
-            interaction.reply("I cannot delete messages that are older than 14 days.");
+            interaction.followUp("I cannot delete messages that are older than 14 days.");
           } else {
-            interaction.reply("There was an error trying to prune messages in this channel!");
+            interaction.followUp("There was an error trying to prune messages in this channel!");
           }
         });
         if (!isNaN(count)) interaction.reply(`Successfully deleted ${count} messages.`);
